ecef9fb86eaecab1e120a82db39679b4fa338b5d
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-02-28 11:58:10 +0100

    add: routes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+// importo react router
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// importo le pagine
+import { HomePage } from "./pages/HomePage";
+import { AboutUs } from "./pages/AboutUs";
+import { PostsList } from "./pages/PostsList";
+
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={HomePage} path="/" />
+        <Route Component={AboutUs} path="/aboutus" />
+        <Route Component={PostsList} path="/postslist" />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
