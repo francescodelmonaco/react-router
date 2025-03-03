8dfa010ecc35c79b0880a4bbbffcd8cdd3f98c69
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-03-03 18:01:24 +0100

    add: responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,14 @@ import { NavLink } from "react-router-dom";
 
 export function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+        <nav className="navbar navbar-expand bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid">
                 <NavLink className="navbar-brand" to="/">React Blog Pages</NavLink>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-                <div className="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse d-flex flex-row-reverse" id="navbarSupportedContent">
                     <div className="navbar-nav">
                         <NavLink
                             className="nav-link"
